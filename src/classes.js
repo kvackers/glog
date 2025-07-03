a23c55939d4a79c96167566a4dafc7b48bb6fbb8
John Doe <bluetoothdoof@proton.me>
2025-07-03 18:43:49 -0300

    added new class

--- src/classes.js
+++ src/classes.js
@@ -20,7 +20,7 @@ function Sacrament({ sacrament }) {
 }
 
 export function Classes() {
-    const [klass, setClass] = useState("fighterA");
+    const [klass, setClass] = useState("wizardA");
 
     const isActive = name => name === klass ? "active" : "";
     const displayClass = name => name === klass ? "" : "d-none";
@@ -28,28 +28,10 @@ export function Classes() {
 
     return html`
         <ul class="nav nav-pills">
-            <li class="nav-item">
-                <a class="nav-link ${isActive("fighterA")}" href="#"
-                   onclick=${() => makeActive("fighterA")}>
-                   Guerreiro
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link ${isActive("fighterB")}" href="#"
-                   onclick=${() => makeActive("fighterB")}>
-                   Ogro
-                </a>
-            </li>
-            <li class="nav-item">
+        <li class="nav-item">
                 <a class="nav-link ${isActive("wizardA")}" href="#"
                    onclick=${() => makeActive("wizardA")}>
                    Arcanista
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link ${isActive("wizardB")}" href="#"
-                   onclick=${() => makeActive("wizardB")}>
-                   Clérigo
                 </a>
             </li>
             <li class="nav-item">
@@ -59,15 +41,33 @@ export function Classes() {
                 </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link ${isActive("geralB")}" href="#"
-                   onclick=${() => makeActive("geralB")}>
-                   Ladino
+                <a class="nav-link ${isActive("wizardB")}" href="#"
+                   onclick=${() => makeActive("wizardB")}>
+                   Clérigo
                 </a>
             </li>
             <li class="nav-item">
                 <a class="nav-link ${isActive("faunaA")}" href="#"
                    onclick=${() => makeActive("faunaA")}>
                    Elfo
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link ${isActive("fighterA")}" href="#"
+                   onclick=${() => makeActive("fighterA")}>
+                   Guerreiro
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link ${isActive("geralB")}" href="#"
+                   onclick=${() => makeActive("geralB")}>
+                   Ladino
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link ${isActive("fighterB")}" href="#"
+                   onclick=${() => makeActive("fighterB")}>
+                   Ogro
                 </a>
             </li>
             <li class="nav-item">
@@ -162,6 +162,8 @@ export function Classes() {
             <h4>Templates</h4>
             <p>Para cada Template de Ogro que você tem, você cresce, ganhando 1 espaço de Inventório preenchido com <strong>Músculos</strong>.</p>
 
+            <p>Esses Templates contam como Templates Raciais.</p>
+
             <ol type="A">
                 <li>Bulking, Esmagar</li>
                 <li>Enorme, Intimidar</li>
@@ -175,6 +177,8 @@ export function Classes() {
             <p>Você pode comer qualquer objeto maior que uma galinha e menos duro que aço como uma Ração. Cada vez que Festejar e cada vez que comer pelo menos 10 rações antes de descansar durante uma aventura, você ganha uma camada de <strong>Músculos</strong> em seu Inventório.</p>
 
             <p>Se comer menos Rações em um dia que o número de <strong>Músculos</strong> que tiver, você perde 1 camada de <strong>Músculos</strong>. Esse é o único jeito de remover <strong>Músculos</strong></p>
+
+            <p>Você não pode escolher esse Template se já tiver um Template Racial.</p>
 
             <h4>Esmagar</h4>
             <p>Quando você usa armas de impacto, seu dado de dano aumenta em 1 passo por padrão, e mais uma vez para cada 5 espaços de Inventório preenchido por <strong>Músculos</strong>.</p>
@@ -308,5 +312,116 @@ export function Classes() {
             <p>Você pode gastar 10 minutos para fazer um pedido direto para as divindades e pedir orientação. Mecanicamente, faça uma pergunta pro GM e ele vai responder de maneira honesta.</p>
 
             <p>Depois disso, a cada nascer do sol que você ver, role 1d4. Se rolar 4, você pode usar habilidade de novo. Caso contrário, o limite que precisa rolar diminui em 1 a cada nascer do sol.</p>
+        </section>
+        
+        <section class="${displayClass("geralA")} pb-3">
+            <h2>Bárbaro</h2>
+
+            <h4>Equipamento Inicial:</h4>
+            <p>Um <strong>Objeto de Casa</strong></p>
+
+            <h4>Perícias Iniciais</h4>
+            <p><strong>Terras Estrangeiras</strong> e uma das opções abaixo:</p>
+            <ul>
+                <li>Explorador</li>
+                <li>Diplomata</li>
+                <li>Refugiado</li>
+            </ul>
+
+            <h4>Templates</h4>
+            <p>Para cada Template de Bárbaro que você ter, você ganha um <strong>Objeto de Casa</strong> a mais (ou seja, você ganha um segundo no Nível 1).</p>
+
+            <ol type="A">
+                <li>Contexto Cultural, Forasteiro</li>
+                <li>Como Você Faz</li>
+                <li>Parágono, -1 Desvantagem Cultural</li>
+                <li>Sincretismo, -1 Desvantagem Cultural</li>
+            </ol>
+
+            <h4>Objeto de Casa</h4>
+            <p>Um <strong>Objeto de Casa</strong> pode ser uma arma, uma peça de armadura, ou uma ferramenta com um propósito específico (que pode ser social). Aos locais, parece estranho e exótico.</p>
+
+            <p>Nas suas mãos, uma Arma de casa tem um dado de dano um passo maior que o equivalente local, uma peça de armadura dá +1 de proteção e você tem vantagem quando usa uma ferramenta de casa.</p>
+
+            <p>Você só pode escolher esse Template se não tiver Templates anteriores, ou se seus Templates anteriores forem raciais.</p>
+
+            <h4>Contexto Cultural</h4>
+            <p>Você se encontra em uma cultura com expectativas muito diferentes do normal. Assim, você consegue fazer coisas que parecem milagrosas para certas pessoas e coisas que parecem muito burras para outras. Role 5 vezes em cada tabela abaixo e escolha 4 resultados, lembrando que os resultados são relativos ao mundo que você se encontra agora.</p>
+
+            <details class="pb-3">
+                <summary>1d20 Vantagens</summary>
+
+                <ol>
+                    <li>Você consegue falar com cavalos <strong>ou</strong> árvores <strong>ou</strong> pessoas inconscientes.</li>
+                    <li>Você tem Perícia em Alquemia</li>
+                    <li>Você tem Perícia em Matemática</li>
+                    <li>Você consegue lutar perfeitamente no escuro, ler lábios e sinais</li>
+                    <li>Você sempre consegue dizer quando alguém está mentindo</li>
+                    <li>Você consegue ver o nível de uma criatura ao ver ela lutar</li>
+                    <li>Você tem Perícia em Economia <strong>ou</strong> Política</li>
+                    <li>Você sempre pode prever o tempo</li>
+                    <li>Você sabe os poderes e fraquezas de anjos e demônios que encontrar</li>
+                    <li>Você encanta e é encantado por fazendeiros <strong>ou</strong> mercantes <strong>ou</strong> ou nobres</li>
+                    <li>Você sempre pode disaparecer em uma multidão</li>
+                    <li>Você consegue andar na água</li>
+                    <li>Você pode mudar sua apresentação de gênero a vontade</li>
+                    <li>Você não deixa rastros e não tem cheiro</li>
+                    <li>Você pode ver perfeitamente na luz das estrelas</li>
+                    <li>Você está imune a maioria das loucuras</li>
+                    <li>Você consegue dormir confortavelmente na maioria das circunstâncias</li>
+                    <li>Você pode identificar a hierarquia social com uma vista rápida</li>
+                    <li>Você reconhece o autor de textos escritos quando o ver em pessoa</li>
+                    <li>No momento mais conveniente, o GM vai anunciar que um NPC acha que você é a alma gêmea dele</li>
+                </ol>
+            </details>
+
+            <details class="pb-3">
+                <summary>1d20 Desvantagens</summary>
+
+                <ol>
+                    <li>Você não entende advogados <strong>ou</strong> nobres <strong>ou</strong> bêbados</li>
+                    <li>Você não entende educação e cortesia</li>
+                    <li>Você não entende reprodução sexual</li>
+                    <li>Você é incapaz de introspecção</li>
+                    <li>Você não entende arte</li>
+                    <li>Você é não reconhece rostos</li>
+                    <li>Você não entende guerra</li>
+                    <li>Você não consegue compreender religião</li>
+                    <li>Você não consegue entender leis</li>
+                    <li>Você tem um juramento contra mercantes <strong>ou</strong> nobres <strong>ou</strong> vagabundos</li>
+                    <li>Você se perde em florestas</li>
+                    <li>Você vai afogar na areia</li>
+                    <li>Você tem um juramento contra couro, pelo e fios de arcos</li>
+                    <li>Você considera Traição tão chulo que você nem consideraria isso como opção</li>
+                    <li>Pássaros <strong>ou</strong> cachorros <strong>ou</strong> rios te odeiam</li>
+                    <li>Ódio é infeccioso para você</li>
+                    <li>Você precisa dormir sem ser visto</li>
+                    <li>Você tem medo daqueles em posição de autoridade social</li>
+                    <li>Você considera falta de educação acreditar que alguém é culpado de algo que não tem testemunhas</li>
+                    <li>No pior possível momento, o GM vai te dizer que um NPC é sua alma gêmea</li>
+                </ol>
+            </details>
+
+            <h4>Forasteiro</h4>
+            <p>É óbvio para todos que te veem que você não é daqui. Você sempre pode encontrar alguém pra te explicar o que está acontecendo, achar pessoas que querem ajuda descartável e pessoas interessadas em culturas estrangeiras. Ou seja, você sempre consegue achar alguém que não te considera uma ameaça em grupos grandes (três ou mais).</p>
+
+            <h4>Como Você Faz</h4>
+            <p>Você consegue consertar um <strong>Objeto de Casa</strong> quebrado em um Descanso Longo. Seus seguidores ganham os mesmos benefícios que você se usarem um de seus <strong>Objetos de Casa</strong>.</p>
+
+            <h4>Parágono</h4>
+            <p>Escolha dois benefícios dos abaixos que batem com Perícias que você tenha:</p>
+            <ul>
+                <li>Se você tiver Perícia em Terras Estrangeiras, você ganha um <strong>Objeto de Casa</strong> toda vez que Festejar</li>
+                <li>Se você tiver Perícia em Explorador, você não precisa mais comer</li>
+                <li>Se você tiver Perícia em Diplomata, você não pode mais ser atacado por criaturas inteligentes se você e seus companheiros não forem hostis.</li>
+                <li>Se você tiver Perícia em Refugiado, você ganha 1 PV por Cicatriz que tiver</li>
+                <li>Se você tiver Perícia em Alquemia, você pode criar uma Poção Aleatória toda vez que fizer um Descanso Longo</li>
+                <li>Se você tiver Perícia em Matemática, você tem sucesso em qualquer teste de DES que fizer com 10 minutos de preparo</li>
+                <li>Se você tiver Perícia em Economia, você ganha um saco de ouro toda vez que Festejar. Você só pode gastar esse ouro com favores políticos.</li>
+                <li>Se você tiver Perícia em Política, escolha uma ideologia. Seus ataques e os de seus seguidores dão 1d6 mais dano se atacarem em nome da ideologia.</li>
+            </ul>
+            
+            <h4>Sincretismo</h4>
+            <p>Você pode escolher o Template A de outra classe e tratar como se fosse simultaneamente um Template dessa classe e de outra.</p>
         </section>`;
 }
